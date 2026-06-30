99ab62f736c840d823873858e61b3fb0c72b3611
everythink98 <231491339+everythink98@users.noreply.github.com>
2026-06-30 08:31:12 +0800

    release: 1.3.35

--- src/appUpdate.test.ts
+++ src/appUpdate.test.ts
@@ -14,9 +14,9 @@ import {
 
 const apkSha256 = 'a'.repeat(64);
 const signerSha256 = 'b'.repeat(64);
-const newerVersion = '1.3.35';
+const newerVersion = '1.3.36';
 const newerTag = `v${newerVersion}`;
-const newerVersionCode = 39;
+const newerVersionCode = 40;
 
 function releaseAssetUrl(tagName: string, assetName: string) {
   return `https://github.com/everythink98/wz-android/releases/download/${tagName}/${assetName}`;
